70c8072c6e7acac783063b87a60b298bf852f97e
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-08 22:57:24 +0600

    test final branch created

--- src/components/Mahadi.jsx
+++ src/components/Mahadi.jsx
@@ -4,6 +4,7 @@ const Mahadi = () => {
       <h1>This is from mahadi</h1>
       <p>created new branch</p>
       <p>Testing branch created</p>
+      <p>testing final branch reated</p>
     </div>
   );
 };
